ca26d689aaf65f32f0d3e13763d25015e4cd1542
Lucas Rockett Gutterman <toucannyc@gmail.com>
2012-08-09 12:55:46 -0400

    404.php no more references

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+	include_once "includes/header.php";
+?>
+<body class="404">
+<header>
+<?php
+	include_once "includes/nav.php";
+?>
+</header>
+<div class="main">
+	<h2>404 Error- There is no page with that name</h2>
+	<p>Please go back to <a href="/">home</a>.</p>
+
+</div>
+<footer>
+<?php
+	include_once "includes/footer.php";
+?>
+</footer>
+</body>
+</html>
